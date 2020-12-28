28868e75db22dfe451f09e153fb3c2d1280f37b0
Jason <jnc1306@gmail.com>
2020-12-28 10:24:25 -0500

    Updates to the netbox script

--- netbox.py
+++ netbox.py
@@ -3,7 +3,8 @@ import pynetbox
 
 # Global Variables
 networks = ['192.168.50.0/24','192.168.25.0/24']
-
+dev_roles = ['Unassigned','Server','Router','Switch','Access Point']
+sites = ['Unassigned','DC1','DC2']
 
 # Netbox API Connection
 nb = pynetbox.api(
@@ -18,37 +19,53 @@ nb.http_session.verify = False
 nm = nmap.PortScanner()
 
 # Scan network with NMAP
-nm.scan('192.168.50.0/24',arguments='-sn')
-
-# Get state of individual ip address
-status = nm['192.168.50.1'].state()
+nm.scan('192.168.50.0/24',arguments='-sP')
 
 def net_scan ():
     for net in networks:
         nm.scan(net,arguments='-sn')
 
-def ip_role (ip):
+def ip_role (host):
     get_ip_addr = nb.ipam.ip_addresses.get(address='{}/24'.format(ip))
     hostname = nm[ip].hostname()
     get_ip_addr.update({'address': '{}/24'.format(ip),
                          'role': 'anycast'})
-def add_ip(ip):
-    get_ip_addr = nb.ipam.ip_addresses.get(address='{}/24'.format(ip))
-    hostname = nm[ip].hostname()
+def add_ip(host):
+    get_ip_addr = nb.ipam.ip_addresses.get(address='{}/24'.format(host))
+    hostname = nm[host].hostname()
     if not get_ip_addr:
-        nb.ipam.ip_addresses.create(address='{}/24'.format(ip),
+        nb.ipam.ip_addresses.create(address='{}/24'.format(host),
                                     dns_name=hostname,
                                     description=hostname)
     else:
-        get_ip_addr.update({'address': '{}/24'.format(ip),
+        get_ip_addr.update({'address': '{}/24'.format(host),
                             'dns_name': hostname,
                             'description': hostname})
 
-def add_device ():
-    
+def add_device (hostname):
+    get_device = nb.dcim.devices.get(name=hostname)
+    if not get_device:
+        nb.dcim.devices.create(name=hostname,
+                               device_role='Unassigned',
+                               site='Unassigned')
 
-for ip in nm.all_hosts():
-    add_ip(ip)
-    ip_role(ip)
+def add_device_role():
+    for role in dev_roles:
+        get_role = nb.dcim.device_roles.get(name=role)
+        if not get_role:
+           nb.dcim.device_roles.create(name=role,
+                                       slug=str(role).lower().replace(' ','_'),
+                                       role='Unassigned')
+
+def add_site():
+    print('')
+
+
+
+if __name__ == '__main__':
+    for host in nm.all_hosts():
+        hostname = nm[host].hostname()
+        #add_device(hostname)
+        add_ip(host)
 
 #print(status, hostname, ip_addr)
